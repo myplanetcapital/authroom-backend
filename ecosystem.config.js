3806f110d680f1b5441a9121a9bab340859fb725
myplanetcapital <admin@myplanetcapital.com>
2026-02-07 16:21:23 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,10 @@
 module.exports = {
     apps : [{
-      name: 'mvmbackend_app',
+      name: 'authroom-backend',
       script: 'index.js',
       out_file: '/dev/null',
       error_file: '/dev/null',
       instances: 3,
-      exec_mode: 'cluster',
-      autorestart: true,
-      max_memory_restart: '600M'
-      
-    },
-    {
-      name: 'app_cron',
-      script: 'app-cron.js',
-      out_file: '/dev/null',
-      error_file: '/dev/null',
-      instances: 1,
       exec_mode: 'cluster',
       autorestart: true,
       max_memory_restart: '600M'
@@ -29,8 +18,8 @@ module.exports = {
         host : ["192.168.0.35"],
         ssh_options: "StrictHostKeyChecking=no",
         ref  : 'origin/main',
-        repo : 'git@github.com:myplanetcapital/myplanet-vehical-management-backend.git',
-        path : '/var/www/backend/myplanet-cab-management',
+        repo : 'git@github.com:myplanetcapital/authroom-backend.git',
+        path : '/var/www/backend/authroom-backend',
         'pre-deploy-local': "echo 'This is a local executed command'",
         'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
         'pre-setup': '',
