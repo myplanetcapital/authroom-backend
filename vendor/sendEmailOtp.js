3efe0ae53f73f9e705e373f35a834ff397c58354
jsol <jsol@universalplus.technology>
2026-02-08 22:50:10 +0530

    update-mail-otp-send

--- vendor/sendEmailOtp.js
+++ vendor/sendEmailOtp.js
@@ -38,6 +38,19 @@ async function sendEmailOtp(email,isUserExists) {
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
+    },
+    tls: {
+      rejectUnauthorized: false // keep this TRUE once cert is fixed
+    }
+  });
+
+  console.log({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === "true", // true for 465
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
     }
   });
 
