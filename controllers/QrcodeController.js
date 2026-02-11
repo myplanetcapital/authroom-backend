bbda8ffbf9e8042745467c15a527f7dc028473ad
jsol <jsol@universalplus.technology>
2026-02-11 15:26:45 +0530

    change-svg-load

--- controllers/QrcodeController.js
+++ controllers/QrcodeController.js
@@ -35,6 +35,28 @@ function decodeOtpauth(otpauth) {
     };
 }
 
+async function convertSvgBase64ToPng(base64Svg) {
+    const base64Data = base64Svg.replace(/^data:image\/svg\+xml;base64,/, "");
+    let svgString = Buffer.from(base64Data, "base64").toString("utf8");
+
+    // If width/height missing → inject them
+    if (!svgString.includes("width=") || !svgString.includes("height=")) {
+        svgString = svgString.replace(
+            "<svg",
+            '<svg width="500" height="500"'
+        );
+    }
+
+    const svgBuffer = Buffer.from(svgString);
+
+    const pngBuffer = await sharp(svgBuffer)
+        .resize(200, 200)
+        .png()
+        .toBuffer();
+
+    return pngBuffer;
+}
+
 exports.generateQRCode = async (req, res) => {
 
     const fieldsValidation = new Validator(req.body, {
@@ -74,6 +96,39 @@ exports.generateQRCode = async (req, res) => {
     let providerAppName = providerData.appName;
     let providerLogo = providerData.logo;
 
+    let logoBuffer;
+
+    if (providerLogo.startsWith("data:image/svg+xml")) {
+        // Base64 SVG
+        const base64Data = providerLogo.replace(/^data:image\/svg\+xml;base64,/, "");
+        const svgBuffer = Buffer.from(base64Data, "base64");
+
+        logoBuffer = await sharp(svgBuffer)
+            .resize(200, 200)
+            .png()
+            .toBuffer();
+
+    } else if (providerLogo.startsWith("http")) {
+        const axios = require("axios");
+        const response = await axios.get(providerLogo, {
+            responseType: "arraybuffer"
+        });
+
+        logoBuffer = await sharp(response.data)
+            .resize(200, 200)
+            .png()
+            .toBuffer();
+
+    } else {
+        const fs = require("fs");
+        const fileBuffer = fs.readFileSync(providerLogo);
+
+        logoBuffer = await sharp(fileBuffer)
+            .resize(200, 200)
+            .png()
+            .toBuffer();
+    }
+
     const secret = authenticator.generateSecret();
     const otpauth = authenticator.keyuri(reqUserName, providerAppName, secret);
 
@@ -82,7 +137,7 @@ exports.generateQRCode = async (req, res) => {
         height: 300,
         shape: "square",
         data: otpauth,
-        image: providerLogo,
+        image: logoBuffer,
         qrOptions: {
             errorCorrectionLevel: "Q",
             margin: 0
@@ -135,7 +190,7 @@ exports.verifyCode = async (req, res) => {
 
     let reqSecret = req.body.secret;
     let code = req.body.code;
-   
+
     const isValid = authenticator.check(code, reqSecret);
 
     return res.status(200).json({
@@ -146,13 +201,13 @@ exports.verifyCode = async (req, res) => {
             'status': true,
         }
     });
-   
+
 
 }
 
 exports.saveData = async (req, res) => {
 
-     const fieldsValidation = new Validator(req.body, {
+    const fieldsValidation = new Validator(req.body, {
         qrData: 'required|string',
         userId: 'required|sometimes|string',
         deviceId: 'required|sometimes|string'
