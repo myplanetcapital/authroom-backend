e3341fdaa67431ad0cbc4cfd3931c2dbe5acffed
jsol <jsol@universalplus.technology>
2026-03-10 12:50:54 +0530

    remove-equalto-origin

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -373,7 +373,7 @@ exports.verifyRegistration = async function (req, res) {
     const verification = await verifyRegistrationResponse({
         response: reqAttestationResponse,
         expectedChallenge: expectedChallenge,
-        expectedOrigin: "android:apk-key-hash:XwPY03hLcxjPEWZYaLORii9VjqjN8ieIQ0YfS6FQru4=",
+        expectedOrigin: "android:apk-key-hash:XwPY03hLcxjPEWZYaLORii9VjqjN8ieIQ0YfS6FQru4",
         expectedRPID: rpID
     });
 
